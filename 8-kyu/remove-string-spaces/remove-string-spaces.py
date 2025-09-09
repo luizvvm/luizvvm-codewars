a0352e67b80e675e95a091ee7927c232094c5209
Luiz Vitor Mattos <luizvvm@dcc.ufrj.br>
2025-09-09 20:02:37 -0300

    Add solution - CodeHub

---
+++ 8-kyu/remove-string-spaces/remove-string-spaces.py
@@ -0,0 +1,5 @@
+def no_space(x):
+    #join: junta uma lista de string em uma só
+    #split: quebra uma lista de string em varias
+    #o "" é necessário para dizer quero "colar" sem nada entre as strings
+    return "".join(x.split())
